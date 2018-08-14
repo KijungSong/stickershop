f497f069130e9d6e4ce43fcbf4cdb422c7ee811a
kijungsong <skj4522@gmail.com>
2018-08-14 23:11:06 +0900

    method query, @Query를 사용한 데이터 조회 예제.

--- src/main/java/com/boot/stickershop/repository/BoardRepository.java
+++ src/main/java/com/boot/stickershop/repository/BoardRepository.java
@@ -9,5 +9,15 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
+    /* method query */
+    public Board findBoardById(Long id);
 
+    public List<Board> findBoardByTitleContaining(String title);
+
+    /* @Query가 붙은 메소드 */
+    @Query("select b from Board b where b.id = :id")
+    public Board queryFindBoardById(@Param("id")Long id);
+
+    @Query("select b from Board b where b.content like %:content%")
+    List<Board> queryFindByContentContaining(@Param("content")String content);
 }
--- src/test/java/com/boot/stickershop/repository/BoardRepositoryTest.java
+++ src/test/java/com/boot/stickershop/repository/BoardRepositoryTest.java
@@ -27,6 +27,19 @@ public class BoardRepositoryTest {
     UserRepository userRepository;
     User user;
 
+    @Before
+    public void initTest() throws Exception {
+        boardCategory = boardCategoryRepository.getOne(1L);
+        user = userRepository.getOne(1L);
+
+        // 3건의 Board 엔티티를 가진다.
+        addBoard("hello spring boot content", "hello spring");
+        addBoard("hello java content", "hello java");
+        addBoard("hello jpa content", "hello jpa");
+    }
+    // 만약 @Test 라고 붙은 메소드가 3개라면 @Before가 붙은 메소드는 몇번 실행될까요?
+    // @Before -> @1Test, @Before -> @2Test ....
+
     @Test
     public void  testAddBoard() throws Exception {
         Board board = new Board();
@@ -41,4 +54,55 @@ public class BoardRepositoryTest {
         System.out.println(saveBoard.getId()); // Assert를 이용한 비교문이 들어가야한다.
     }
 
+    private void addBoard(String content, String title) {
+        Board board = new Board();
+        board.setBoardCategory(boardCategory);
+        board.setUser(user);
+        board.setContent(content);
+        board.setEdittime(LocalDateTime.now());
+        board.setHit(0);
+        board.setTitle(title);
+
+        Board saveBoard = boardRepository.save(board);
+        System.out.println(saveBoard.getId()); // Assert를 이용한 비교문이 들어가야한다.
+    }
+
+    @Test
+    public void testGetBoardId1() throws Exception {
+        Board board = boardRepository.getOne(1L);
+        System.out.println(board.getTitle());
+        System.out.println(board.getContent());
+    }
+
+    @Test
+    public void testGetBoardId2() throws Exception {
+        Board board = boardRepository.findBoardById(1L);
+        System.out.println(board.getTitle());
+        System.out.println(board.getContent());
+    }
+
+    // 제목에 llo가 포함된 목록을 구하는 메소드를 boardRepository에 추가하시오.
+    @Test
+    public void testFindBoardByTitleContaining() throws Exception {
+        List<Board> boardList = boardRepository.findBoardByTitleContaining("llo");
+        for(Board b : boardList) {
+            System.out.println(b.getTitle());
+        }
+    }
+
+    // @Query를 이용해서 id가 일치하는 Board를 반환하는 메소드를 BoardRepository에 추가하시오.
+    @Test
+    public void testQueryFindBoardById() throws Exception {
+        Board board = boardRepository.queryFindBoardById(1L);
+        System.out.println(board.getTitle());
+        System.out.println(board.getContent());
+    }
+
+    @Test
+    public void testQueryFindByContentContaining() throws Exception {
+        List<Board> boardList = boardRepository.queryFindByContentContaining("llo");
+        for(Board b : boardList) {
+            System.out.println(b.getTitle());
+        }
+    }
 }
